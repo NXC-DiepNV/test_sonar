68a59a51eb8c20394e7b0d3b13846b865ba7c9d5
Ngo Van Diep <nxt-tranducanh@syp.vn>
2024-08-06 15:52:25 +0700

    test 20

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+<?php 
+    $password = '12345';
+ ?>
 <body>
-    <h1>DiepNV test 1</h1>
+    <h1>DiepNV test 1 <?php echo $password; ?></h1>
 </body>
+
+
 </html>
