293844b9d4068c8072917b29338cdb828ad5058d
Ngo Van Diep <nxt-tranducanh@syp.vn>
2024-08-07 08:52:52 +0700

    sonar 3

--- index.php
+++ index.php
@@ -6,15 +6,27 @@
     <title>Document</title>
 </head>
 
-<?php 
-    $password = '12345';
+<?php
+// Lỗi SQL Injection
+$userInput = $_GET['user'];
+$query = "SELECT * FROM users WHERE username = '$userInput'";
+$result = mysqli_query($conn, $query);
 
-    function abcd() {
-        return 1;
-    }
- ?>
+// Lỗi sử dụng biến chưa được khởi tạo
+echo $uninitializedVariable;
+
+// Lỗi không kiểm tra kết quả của hàm fopen
+$file = fopen("test.txt", "r");
+
+// Lỗi XSS
+$userInput = $_GET['userInput'];
+echo "<div>$userInput</div>";
+
+// Lỗi hardcoded credentials
+$dbPassword = 'hardcodedPassword';
+?>
 <body>
-    <h1>DiepNV test 1 <?php echo $password; if (abc() == 2) { echo 'ABC';} ?></h1>
+    <h1>DiepNV test 1 <?php echo $password; ?></h1>
 </body>
 
 
