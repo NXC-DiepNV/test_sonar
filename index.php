6d90d5e157caaba5542b99bafff2a35849a2e535
Ngo Van Diep <nxt-tranducanh@syp.vn>
2024-08-08 15:00:19 +0700

    main 17

--- index.php
+++ index.php
@@ -7,6 +7,55 @@
 </head>
 
 <?php
+// Lỗi SQL Injection
+$userInput = $_GET['user'];
+$query = "SELECT * FROM users WHERE username = '$userInput'";
+$result = mysqli_query($conn, $query);
+
+// Lỗi sử dụng biến chưa được khởi tạo
+echo $uninitializedVariable;
+
+// Lỗi không kiểm tra kết quả của hàm fopen
+$file = fopen("test.txt", "r");
+
+// Lỗi XSS
+$userInput = $_GET['userInput'];
+echo "<div>$userInput</div>";
+
+// Lỗi hardcoded credentials
+$dbPassword = 'hardcodedPassword';
+
+$this = 1;
+
+$text = '';
+
+$text =+ '12';
+
+
+if ($b == 0) {  // Noncompliant
+    doOneMoreThing();
+  } else {
+    doOneMoreThing();
+  }
+  
+  $b = $a > 12 ? 4 : 4;  // Noncompliant
+  
+  switch ($i) {  // Noncompliant
+    case 1:
+      doSomething();
+      break;
+    case 2:
+      doSomething();
+      break;
+    case 3:
+      doSomething();
+      break;
+    default:
+      doSomething();
+  }
+
+  for ($i = 0; $i < $length; $i--) 
+
 
 ?>
 <body>
